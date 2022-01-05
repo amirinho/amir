fa464268cc6e8462ba213c366d6ed1ab193532a7
amirinho <75517621+amirinho@users.noreply.github.com>
2022-01-05 10:59:13 +0200

    Update sever.c

--- sever.c
+++ sever.c
@@ -1,1 +1,16 @@
 
+  main.c
+//  amirsocketC
+//
+//  Created by admin on 4.01.2022.
+//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#define SIZE 1024
+#define PORT 5000
+
+ 
